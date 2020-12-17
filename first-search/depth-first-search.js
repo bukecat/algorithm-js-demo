62ed7e5193eabfba39e4ddb8500b18c2525e7642
bukecat <342231678@qq.com>
2020-12-17 23:51:33 +0800

    -

--- first-search/depth-first-search.js
+++ first-search/depth-first-search.js
@@ -1,17 +1,37 @@
 const data = require('./constants');
 
-function dfs(list) {
-	const path = [];
-	
-	list.forEach((node) => {
-		const search = (node) => {
-			path.push(node.name);
-			
-			if (node.children) {
-				search(node.children);
-			}
+function dfs1(node, path = []) {
+	path.push(node.name);
+
+	if (node.children) {
+		node.children.forEach((child) => {
+			dfs1(child, path);
+		})
+	}
+
+	return path;
+}
+console.log(dfs1(data));
+
+
+
+let dfs2 = (node, nodeList = []) => {
+	if (node !== null) {
+		nodeList.push(node.name)
+		let children = node.children || [];
+		for (let i = 0; i < children.length; i++) {
+			dfs2(children[i], nodeList)
 		}
-	})
-	
-	
+	}
+	return nodeList
+}
+console.log(dfs2(data));
+
+
+
+let dfs3 = () => {
+	const stack = [];
+	const path = [];
+
+	stack.push()
 }
